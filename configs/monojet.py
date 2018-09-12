00959e859269a30ca4bc6d1c408121500a6df681
Sonaina Undleeb <sonaina.undleeb@ttu.edu>
2018-09-12 12:40:05 -0500

    monojet config fixed

--- configs/monojet.py
+++ configs/monojet.py
@@ -27,7 +27,8 @@ for s in ['0tag','1tag', '2tag']:
      monojet_category[s] = {
         'name':"monojet_"+s
         #,'in_file_name':"/uscms_data/d1/shoh/panda/v_8029_DarkHiggs_v2/flat/limits/fittingForest_monojet_"+s+".root"
-        ,'in_file_name':"/uscms/home/naina25/nobackup/Panda_2018/Panda_Analysis/CMSSW_8_0_29/src/PandaAnalysis/SuperMonoJet/fitting/fittingForest_monojet_"+s+".root"
+       # ,'in_file_name':"/uscms/home/naina25/nobackup/Panda_2018/Panda_Analysis/CMSSW_8_0_29/src/PandaAnalysis/SuperMonoJet/fitting/fittingForest_monojet_"+s+".root"
+        ,'in_file_name':"/uscms_data/d3/naina25/panda/80X-v1.5-monojet/fittingForest_"+s+".root"
         ,"cutstring":""
         ,"varstring":["min(999.9999,met)",250,1250]
         ,"weightname":"weight"
@@ -37,79 +38,54 @@ for s in ['0tag','1tag', '2tag']:
 	,"samples":
              {  
 		  # Signal Region
-#		   "VH_signal"    	       :['signal'+s,'vh',1,0]
-		  "Zvv_signal"    	       :['signal'+s,'zjets',1,0]
-                  ,"Zll_signal"	               :['signal'+s,'zll',1,0]
- 		  ,"Wlv_signal"  	       :['signal'+s,'wjets',1,0]
-		  ,"Diboson_signal"  	       :['signal'+s,'dibosons',1,0]
-		  ,"ttbar_signal"   	       :['signal'+s,'ttbar',1,0]
-		  ,"ST_signal"                 :['signal'+s,'stop',1,0]
-		  ,"QCD_signal"		       :['signal'+s,'qcd',1,0]
-		  ,"GJets_signal"	       :['signal'+s,'gjets',1,0]
-		  ,"Data_signal"	       :['signal'+s,'data',0,0]
+#		   "VH_signal_"+s    	       :['signal'+s,'vh',1,0]
+		  "Zvv_signal_"+s    	       :['signal'+s,'zjets',1,0]
+                  ,"Zll_signal_"+s	               :['signal'+s,'zll',1,0]
+ 		  ,"Wlv_signal_"+s  	       :['signal'+s,'wjets',1,0]
+		  ,"Diboson_signal_"+s  	       :['signal'+s,'dibosons',1,0]
+		  ,"ttbar_signal_"+s   	       :['signal'+s,'ttbar',1,0]
+		  ,"ST_signal_"+s                 :['signal'+s,'stop',1,0]
+		  ,"QCD_signal_"+s		       :['signal'+s,'qcd',1,0]
+		  ,"Data_signal_"+s	       :['signal'+s,'data',0,0]
 		  # signals
-                  ,"hsDM_1000_50_100_signal"    :['signal'+s,'hsDM-1000-50-100_signal',1,1]
-                  ,"hsDM_1000_50_200_signal"    :['signal'+s,'hsDM-1000-50-200_signal',1,1]
-                  ,"hsDM_1000_50_250_signal"    :['signal'+s,'hsDM-1000-50-250_signal',1,1]
-                  ,"hsDM_1000_50_300_signal"    :['signal'+s,'hsDM-1000-50-300_signal',1,1]
-                  
-                  ,"ZpDM_1000_150_10_signal"    :['signal'+s,'ZpDM-1000-150-10_signal',1,1]
-                  ,"ZpDM_1000_50_10_signal"    :['signal'+s,'ZpDM-1000-50-10_signal',1,1]
-                  ,"ZpDM_100_150_10_signal"    :['signal'+s,'ZpDM-100-150-10_signal',1,1]
-                  ,"ZpDM_100_50_10_signal"    :['signal'+s,'ZpDM-100-50-10_signal',1,1]
-                  ,"ZpDM_1500_150_10_signal"    :['signal'+s,'ZpDM-1500-150-10_signal',1,1]
-                  ,"ZpDM_1500_50_10_signal"    :['signal'+s,'ZpDM-1500-50-10_signal',1,1]
-                  ,"ZpDM_2000_150_10_signal"    :['signal'+s,'ZpDM-2000-150-10_signal',1,1]
-                  ,"ZpDM_2000_50_10_signal"    :['signal'+s,'ZpDM-2000-50-10_signal',1,1]
-                  ,"ZpDM_2500_150_10_signal"    :['signal'+s,'ZpDM-2500-150-10_signal',1,1]
-                  ,"ZpDM_3000_150_10_signal"    :['signal'+s,'ZpDM-3000-150-10_signal',1,1]
-                  ,"ZpDM_3000_50_10_signal"    :['signal'+s,'ZpDM-3000-50-10_signal',1,1]
-                  ,"ZpDM_300_150_10_signal"    :['signal'+s,'ZpDM-300-150-10_signal',1,1]
 
 		  # Di muon-Control
-#                  ,"VH_zmm"                    :['dimuon'+s,'vh',1,0] 
-                  ,"Zll_zmm"	               :['dimuon'+s,'zll',1,1]
- 		  ,"Wlv_zmm"      	       :['dimuon'+s,'wjets',1,0]
-		  ,"Diboson_zmm"    	       :['dimuon'+s,'dibosons',1,0]
-		  ,"ttbar_zmm"    	       :['dimuon'+s,'ttbar',1,0]
-		  ,"ST_zmm"                    :['dimuon'+s,'stop',1,0]
-		  ,"QCD_zmm"		       :['dimuon'+s,'qcd',1,0]
-		  ,"Data_zmm"    	       :['dimuon'+s,'data',0,0]
+#                  ,"VH_zmm_"+s                    :['dimuon'+s,'vh',1,0] 
+                  ,"Zll_zmm_"+s	               :['dimuon'+s,'zll',1,1]
+		  ,"Diboson_zmm_"+s    	       :['dimuon'+s,'dibosons',1,0]
+		  ,"ttbar_zmm_"+s    	       :['dimuon'+s,'ttbar',1,0]
+		  ,"Data_zmm_"+s    	       :['dimuon'+s,'data',0,0]
 
                   # Di electron-Control
-#                  ,"VH_zee"                    :['dielectron'+s,'vh',1,0] 
-                  ,"Zll_zee"                   :['dielectron'+s,'zll',1,1]
-                  ,"Wlv_zee"                   :['dielectron'+s,'wjets',1,0]
-                  ,"Diboson_zee"               :['dielectron'+s,'dibosons',1,0]
-                  ,"ttbar_zee"                 :['dielectron'+s,'ttbar',1,0]
-                  ,"ST_zee"                    :['dielectron'+s,'stop',1,0]
-                  ,"QCD_zee"                   :['dielectron'+s,'qcd',1,0]
-                  ,"Data_zee"                  :['dielectron'+s,'data',0,0]
+#                  ,"VH_zee_"+s                    :['dielectron'+s,'vh',1,0] 
+                  ,"Zll_zee_"+s                   :['dielectron'+s,'zll',1,1]
+                  ,"Diboson_zee_"+s               :['dielectron'+s,'dibosons',1,0]
+                  ,"ttbar_zee_"+s                 :['dielectron'+s,'ttbar',1,0]
+                  ,"Data_zee_"+s                  :['dielectron'+s,'data',0,0]
 
                    # Single muon (w) control
 #                  ,"VH_mn"                    :['singlemuon'+s,'vh',1,0] 
-                  ,"Zll_mn"                   :['singlemuon'+s,'zll',1,0]
-                  ,"Wlv_mn"                   :['singlemuon'+s,'wjets',1,1]
-                  ,"Diboson_mn"               :['singlemuon'+s,'dibosons',1,0]
-                  ,"ttbar_mn"                 :['singlemuon'+s,'ttbar',1,0]
-                  ,"ST_mn"                    :['singlemuon'+s,'stop',1,0]
-                  ,"QCD_mn"                   :['singlemuon'+s,'qcd',1,0]
-                  ,"Data_mn"                  :['singlemuon'+s,'data',0,0]
+                  ,"Zll_wmn_"+s                   :['singlemuon'+s,'zll',1,0]
+                  ,"Wlv_wmn_"+s                   :['singlemuon'+s,'wjets',1,1]
+                  ,"Diboson_wmn_"+s               :['singlemuon'+s,'dibosons',1,0]
+                  ,"ttbar_wmn_"+s                 :['singlemuon'+s,'ttbar',1,0]
+                  ,"QCD_wmn_"+s                   :['singlemuon'+s,'qcd',1,0]
+                  ,"Data_wmn_"+s                  :['singlemuon'+s,'data',0,0]
 
                    # Single electron (w) control
 #                  ,"VH_en"                    :['singleelectron'+s,'vh',1,0] 
-                  ,"Zll_en"                   :['singleelectron'+s,'zll',1,0]
- 		  ,"Wlv_en"                   :['singleelectron'+s,'wjets',1,1]
-		  ,"Diboson_en"               :['singleelectron'+s,'dibosons',1,0]
-		  ,"ttbar_en"                 :['singleelectron'+s,'ttbar',1,1]
-		  ,"ST_en"                    :['singleelectron'+s,'stop',1,0]
-		  ,"QCD_en"                   :['singleelectron'+s,'qcd',1,0]
-		  ,"Data_en"                  :['singleelectron'+s,'data',0,0]
+                  ,"Zll_wen_"+s                   :['singleelectron'+s,'zll',1,0]
+ 		  ,"Wlv_wen_"+s                   :['singleelectron'+s,'wjets',1,1]
+		  ,"Diboson_wen_"+s               :['singleelectron'+s,'dibosons',1,0]
+		  ,"ttbar_wen_"+s                 :['singleelectron'+s,'ttbar',1,1]
+		  ,"ST_wen_"+s                    :['singleelectron'+s,'stop',1,0]
+		  ,"QCD_wen_"+s                   :['singleelectron'+s,'qcd',1,0]
+		  ,"Data_wen_"+s                  :['singleelectron'+s,'data',0,0]
 
                    # Single photon control
-                  ,"GJets_gjets"                 :['singlephoton'+s,'gjets',1,1]
-                  ,"QCD_gjets"                   :['singlephoton'+s,'qcd',1,0]
-                  ,"Data_gjets"                  :['singlephoton'+s,'data',0,0]
+                  ,"Pho_pho_"+s                 :['singlephoton'+s,'gjets',1,1]
+                  ,"QCD_pho_"+s                   :['singlephoton'+s,'qcd',1,0]
+                  ,"Data_pho_"+s                  :['singlephoton'+s,'data',0,0]
                   }
 
         }
